3e14dd3b1d450e4a3c371c94dc4564840480dc3a
Tatsuya-kanaria <kanaria.git@gmail.com>
2022-08-01 16:04:37 +0900

    finish D56take_out.py

---
+++ src/Data_processing/4_language_data/D56take_out.py
@@ -0,0 +1,16 @@
+# %%
+
+import D55mecab_dt as D55
+
+
+mecab_df = D55.mecab_df
+
+print(mecab_df.groupby(['原形', '品詞']).size().sort_values(ascending=False))
+
+noun = mecab_df.loc[mecab_df['品詞'] == '名詞']
+# noun
+
+verb = mecab_df.loc[(mecab_df['品詞'] == '名詞') | (mecab_df['品詞'] == '動詞')]
+# verb
+
+# %%
