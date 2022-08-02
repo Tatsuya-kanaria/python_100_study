4bf60970e6a65a9f1ee09ceec76313ebf96bd740
Tatsuya-kanaria <kanaria.git@gmail.com>
2022-08-02 21:13:03 +0900

    modify import D57stop_words

--- src/Data_processing/4_language_data/D58matplotlib.py
+++ src/Data_processing/4_language_data/D58matplotlib.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import japanize_matplotlib
 
-import D56take_out as D56
+import D57stop_words as D57
 
-count = D56.noun.groupby('原形').size().sort_values(ascending=False)
+count = D57.verb.groupby('原形').size().sort_values(ascending=False)
 count.name = 'count'
 count = count.reset_index().head(10)
 count
