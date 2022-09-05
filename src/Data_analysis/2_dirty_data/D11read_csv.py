1feb3ab9afd736783df8cb6b167402da8a4ae0fc
Tatsuya-kanaria <kanaria.git@gmail.com>
2022-09-05 22:16:43 +0900

    finish D11read_csv.py

---
+++ src/Data_analysis/2_dirty_data/D11read_csv.py
@@ -0,0 +1,11 @@
+# %%
+import pandas as pd
+
+
+uriage_data = pd.read_csv('./data/uriage.csv')
+uriage_data.head()
+
+kokyaku_data = pd.read_excel('./data/kokyaku_daicho.xlsx')
+kokyaku_data.head()
+
+# %%
