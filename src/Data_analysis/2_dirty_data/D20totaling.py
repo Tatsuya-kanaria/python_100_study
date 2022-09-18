b84c8a98ed5c3f1e5179bcb89c9ff08b27954605
Tatsuya-kanaria <kanaria.git@gmail.com>
2022-09-18 09:26:38 +0900

    finish D20totaling.py

--- src/Data_analysis/2_dirty_data/D20totaling.py
+++ src/Data_analysis/2_dirty_data/D20totaling.py
@@ -1,6 +1,8 @@
 # %%
 import pandas as pd
 
+import D11read_csv as D11
+import D18merge
 
 import_data = pd.read_csv("./data/dump_data.csv")
 import_data
@@ -14,6 +16,17 @@ byPrice = import_data.pivot_table(
     index="purchase_month", columns="item_name", values="item_price", aggfunc="sum", fill_value=0)
 byPrice
 
+byCustomer = import_data.pivot_table(
+    index="purchase_month", columns="顧客名", aggfunc="size", fill_value=0)
+byCustomer
 
+byRegion = import_data.pivot_table(
+    index="purchase_month", columns="地域", aggfunc="size", fill_value=0)
+byRegion
+
+# 集計期間に購入していない
+away_data = pd.merge(D11.uriage_data, D11.kokyaku_data,
+                     left_on="customer_name", right_on="顧客名", how="right")
+away_data[away_data["purchase_date"].isnull()][["顧客名", "メールアドレス", "登録日"]]
 
 # %%
