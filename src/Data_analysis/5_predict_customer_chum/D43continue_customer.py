22c6b385d9264948c13e81f03790a1937368d86d
Tatsuya-kanaria <kanaria.git@gmail.com>
2022-10-07 23:41:46 +0900

    D43 add comment

--- src/Data_analysis/5_predict_customer_chum/D43continue_customer.py
+++ src/Data_analysis/5_predict_customer_chum/D43continue_customer.py
@@ -17,13 +17,16 @@ conti_uselog = conti_uselog.dropna(subset=["name"])
 if __name__ == '__main__':
     print(len(conti_uselog))
 
+# シャッフル
 conti_uselog = conti_uselog.sample(frac=1).reset_index(drop=True)
+# 重複削除
 conti_uselog = conti_uselog.drop_duplicates(subset="customer_id")
 
 if __name__ == '__main__':
     print(len(conti_uselog))
 conti_uselog.head()
 
+# 継続顧客と退会顧客を縦に結合
 predict_data = pd.concat([conti_uselog, D42.exit_uselog], ignore_index=True)
 
 if __name__ == '__main__':
