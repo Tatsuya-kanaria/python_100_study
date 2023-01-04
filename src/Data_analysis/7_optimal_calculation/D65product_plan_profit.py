8ea911f4cc152927b5b8c723b3b0756e50c7d28a
Tatsuya-kanaria <kanaria.git@gmail.com>
2023-01-04 18:02:34 +0900

    finish D65product_plan_profit.py

---
+++ src/Data_analysis/7_optimal_calculation/D65product_plan_profit.py
@@ -0,0 +1,15 @@
+# %%
+import D64read_plan as D64
+
+
+def product_plan(df_profit, df_plan):
+    profit = 0
+    for i in range(len(df_profit.index)):
+        for j in range(len(df_plan.columns)):
+            profit += df_profit.iloc[i][j] * df_plan.iloc[i][j]
+    return profit
+
+
+print("総利益: " + str(product_plan(D64.df_profit, D64.df_plan)))
+
+# %%
