858840d8f6e710fa74c4a882075374785595254c
Tatsuya-kanaria <kanaria.git@gmail.com>
2023-01-27 20:58:14 +0900

    finish D16visulization_of_sales_by_prefecture.py

---
+++ src/Machine_learning_system/2_visualization/D16visalization_of_sales_by_prefecture.py
@@ -0,0 +1,25 @@
+# %%
+import japanize_matplotlib
+import D13sales_by_month as D13
+
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+pre_data = pd.pivot_table(D13.analyze_data, index='order_accept_month',
+                          columns='narrow_area', values='total_amount', aggfunc='mean')
+
+pre_data
+
+'県名を'
+narrow_area = set(D13.analyze_data['narrow_area'])
+
+if __name__ == '__main__':
+    print(narrow_area)
+
+for area in narrow_area:
+    plt.plot(list(pre_data.index), pre_data[area], label=area)
+
+plt.legend()
+
+# %%
