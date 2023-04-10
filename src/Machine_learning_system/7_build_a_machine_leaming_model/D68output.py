7374c129e9712af007aa2f7d9af988a707a1ddf4
Tatsuya-kanaria <kanaria.git@gmail.com>
2023-04-10 17:21:26 +0900

    finish D68output.py

--- src/Machine_learning_system/7_build_a_machine_leaming_model/D68output.py
+++ src/Machine_learning_system/7_build_a_machine_leaming_model/D68output.py
@@ -7,7 +7,7 @@ from D61data_read import output_dir
 from D67functionalization import score, importance, cols, model
 
 
-now = datetime.datetime.now().strftime("%Y%m%d%H%M%s")
+now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 target_output_dir_name = "results_" + now
 target_output_dir = os.path.join(output_dir, target_output_dir_name)
 os.makedirs(target_output_dir, exist_ok=True)
