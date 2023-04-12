155fb7d01e85d947ba0eba79e04dd442d997d34c
Tatsuya-kanaria <kanaria.git@gmail.com>
2023-04-12 19:53:10 +0900

    type correction

--- src/Machine_learning_system/9_small_machinelearning_system/D81folder_creation_and_initial_variables.py
+++ src/Machine_learning_system/9_small_machinelearning_system/D81folder_creation_and_initial_variables.py
@@ -5,7 +5,7 @@ import os
 data_dir = 'data'
 
 input_dir = os.path.join(data_dir, '00_input')
-score_monthly_dir = os.path.join(data_dir, '01_store_monthly')
+store_monthly_dir = os.path.join(data_dir, '01_store_monthly')
 ml_base_dir = os.path.join(data_dir, '02_ml_base')
 
 output_ml_result_dir = os.path.join(data_dir, '10_output_ml_result')
@@ -15,7 +15,7 @@ master_dir = os.path.join(data_dir, '99_master')
 model_dir = 'models'
 
 os.makedirs(input_dir, exist_ok=True)
-os.makedirs(score_monthly_dir, exist_ok=True)
+os.makedirs(store_monthly_dir, exist_ok=True)
 os.makedirs(ml_base_dir, exist_ok=True)
 os.makedirs(output_ml_result_dir, exist_ok=True)
 os.makedirs(output_report_dir, exist_ok=True)
