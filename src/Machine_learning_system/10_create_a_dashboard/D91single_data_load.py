9f6ca426da4ae31b9e50c90ebfc808f6e79b5ffc
Tatsuya-kanaria <kanaria.git@gmail.com>
2023-04-27 20:16:24 +0900

    finish D91single_data_load.py

---
+++ src/Machine_learning_system/10_create_a_dashboard/D91single_data_load.py
@@ -0,0 +1,23 @@
+# %%
+from IPython.core.display import display
+import os
+import pandas as pd
+
+data_dir = 'data'
+store_monthly_dir = os.path.join(data_dir, '01_store_monthly')
+ml_base_dir = os.path.join(data_dir, '02_ml_base')
+
+output_ml_result_dir = os.path.join(data_dir, '10_output_ml_result')
+output_report_dir = os.path.join(data_dir, '11_output_report')
+
+store_monthly_file = 'store_monthly_data.csv'
+ml_base_file = 'ml_base_data.csv'
+
+store_monthly_data = pd.read_csv(
+    os.path.join(store_monthly_dir, store_monthly_file))
+ml_base_data = pd.read_csv(os.path.join(ml_base_dir, ml_base_file))
+
+display(store_monthly_data.head(3))
+display(ml_base_data.head(3))
+
+# %%
