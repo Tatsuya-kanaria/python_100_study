5c5a81dc0933e4cd8c4cc123b6e7bf3bfa909a7d
Tatsuya-kanaria <kanaria.git@gmail.com>
2023-05-14 11:30:44 +0900

    add EnemyManagerTestCase, UtiltyRPGTestCase

--- src/rpg/unit_test.py
+++ src/rpg/unit_test.py
@@ -1,8 +1,12 @@
 import unittest
+from unittest.mock import patch
+from io import StringIO
+from contextlib import redirect_stdout
 from test.support import captured_stdout
+import sys
 
 
-from RPG import Character, Player, HealingItem, AttackItem, RecoveryAgents, Bom, EnemyManager, Enemy, Goblin, Orc, Slime, choose_item, reset_game, print_status, main
+from RPG import Character, Player, HealingItem, AttackItem, RecoveryAgents, Bom, EnemyManager, Enemy, Goblin, Orc, Slime, get_valid_input, choose_item, reset_game, print_status, main
 
 
 class CharacterTestCase(unittest.TestCase):
@@ -160,7 +164,7 @@ class EnemyTestCase(unittest.TestCase):
         self.assertEqual(self.enemy.exp_reward, 15, "敵の経験値が正しいこと")
 
 
-class HealingItemEnemyTestCase(unittest.TestCase):
+class HealingItemTestCase(unittest.TestCase):
     def setUp(self):
         self.Item = HealingItem(
             name="アイテム", description="説明", restore_amount=20)
@@ -181,7 +185,7 @@ class HealingItemEnemyTestCase(unittest.TestCase):
         self.assertEqual(self.Item.kind, "回復", "回復アイテムの種類が正しいこと")
 
 
-class AttackItemEnemyTestCase(unittest.TestCase):
+class AttackItemTestCase(unittest.TestCase):
     def setUp(self):
         self.Item = AttackItem(
             name="アイテム", description="説明", attack_damage=50)
@@ -200,6 +204,140 @@ class AttackItemEnemyTestCase(unittest.TestCase):
 
     def test_kind(self):
         self.assertEqual(self.Item.kind, "攻撃", "攻撃アイテムの種類が正しいこと")
+
+
+class EnemyManagerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.enemy_manager = EnemyManager()
+
+    def tearDown(self):
+        self.enemy_manager = None
+
+    def test_add_enemy(self):
+        enemy = Goblin()
+        self.enemy_manager.add_enemy(enemy)
+
+        self.assertIn(enemy, self.enemy_manager.enemies, "敵が正しく追加されていること")
+
+    def test_next_enemy_with_enemies(self):
+        goblin = Goblin()
+        orc = Orc()
+        slime = Slime()
+
+        self.enemy_manager.add_enemy(goblin)
+        self.enemy_manager.add_enemy(orc)
+        self.enemy_manager.add_enemy(slime)
+
+        with captured_stdout() as stdout:
+            self.enemy_manager.next_enemy()
+
+            self.assertEqual(self.enemy_manager.current_enemy,
+                             goblin, "敵が正しく設定されていること")
+            self.assertNotIn(
+                goblin, self.enemy_manager.enemies, "リストから敵だ取り除かれていること")
+            self.assertEqual(stdout.getvalue(),
+                             f"{goblin.name}が現れた！\n", "正しいメッセージが追加されていること")
+
+    def test_next_enemy_without_enemies(self):
+        with captured_stdout() as stdout:
+            self.enemy_manager.next_enemy()
+
+            self.assertIsNone(self.enemy_manager.current_enemy,
+                              "敵がいない時Noneが設定されていること")
+            self.assertEqual(stdout.getvalue(), "", "出力メッセージがないこと")
+
+    def test_reset(self):
+        self.enemy_manager.add_enemy(Goblin())
+        self.enemy_manager.add_enemy(Orc())
+        self.enemy_manager.add_enemy(Slime())
+
+        with captured_stdout() as stdout:
+            self.enemy_manager.next_enemy()  # リストから敵を取り出す
+
+            self.enemy_manager.reset()
+
+            self.assertEqual(len(self.enemy_manager.enemies),
+                             3, "敵リストが正しく初期化されていること")
+            self.assertIsNone(self.enemy_manager.current_enemy,
+                              "現在の敵がNoneにリセットされていること")
+
+    def test_is_defeated_with_enemies(self):
+        self.enemy_manager.add_enemy(Goblin())
+        self.enemy_manager.add_enemy(Orc())
+        self.enemy_manager.add_enemy(Slime())
+
+        self.assertFalse(self.enemy_manager.is_defeated(),
+                         "敵がいる場合はFalseが返されること")
+
+    def test_is_defeated_without_enemies(self):
+        self.assertTrue(self.enemy_manager.is_defeated(),
+                        "敵がいない場合はTrueが返されること")
+
+
+class UtilityRPGTestCase(unittest.TestCase):
+    @unittest.skip("無限ループから抜けなくなるため、手動でテストする")
+    @patch('builtins.input', side_effect=['2'])
+    def test_get_valid_input(self, mock_input):
+        expected_output = "無効な選択です。再度入力してください。\n選択肢を入力してください: "
+        mock_input.__iter__.return_value = ['2']
+
+        stdout = StringIO()
+
+        # with redirect_stdout(StringIO()) as stdout:
+        with patch('sys.stdout', stdout):
+            choice = get_valid_input(["1", "3", "5"])
+
+        self.assertEqual(choice, "2", "正しい選択が返されること")
+        self.assertEqual(stdout.getvalue(), expected_output,
+                         "適切なメッセージが表示されること")
+
+    @patch('builtins.input', return_value='1')
+    def test_choose_item(self, mock_input):
+        items = [
+            RecoveryAgents(),
+            Bom()
+        ]
+
+        with redirect_stdout(StringIO()) as stdout:
+            chosen_item = choose_item(items)
+
+        self.assertEqual(chosen_item, items[0], "正しいアイテムが選択されること")
+        self.assertIn(items[0].name, stdout.getvalue(), "選択されたアイテムが表示されること")
+
+    @patch('builtins.input', return_value='0')
+    def test_choose_item_cancel(self, mock_input):
+        expected_output = "使用するアイテムを選んでください:\n1. 回復薬 - 体力を20回復するアイテム\n2. 爆弾 - 敵に50ダメージを与えるアイテム\n0. キャンセル\n"
+        items = [
+            RecoveryAgents(),
+            Bom()
+        ]
+
+        with redirect_stdout(StringIO()) as stdout:
+            chosen_item = choose_item(items)
+
+        self.assertIsNone(chosen_item, "キャンセル時にNoneが返されること")
+        self.assertEqual(stdout.getvalue(), expected_output, "正しい選択肢が表示されること")
+
+    def test_reset_game(self):
+        player = Player("勇者", 100, 20, 10)
+        enemy_manager = EnemyManager()
+
+        reset_game(player, enemy_manager)
+
+        self.assertEqual(len(player.items), 3, "プレイヤーのアイテムが初期化されること")
+        self.assertEqual(len(enemy_manager.enemies), 3, "敵のリストが初期化されること")
+
+    def test_print_status(self):
+        player = Player("勇者", 100, 20, 10)
+        enemy = Enemy("ゴブリン", 30, 5, 8, 15)
+
+        expected_output = "勇者: HP 100, MP 20\nゴブリン: HP 30\n"
+
+        with redirect_stdout(StringIO()) as stdout:
+            print_status(player, enemy)
+
+        self.assertEqual(stdout.getvalue(), expected_output,
+                         "正しいステータスが表示されること")
 
 
 if __name__ == '__main__':
