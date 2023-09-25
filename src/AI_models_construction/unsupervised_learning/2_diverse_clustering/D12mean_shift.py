28e4e3e0b3cf46d62784df2336f00a109c970d5d
Tatsuya-kanaria <kanaria.git@gmail.com>
2023-09-25 15:46:19 +0900

    Refactoring commonization

--- src/AI_models_construction/unsupervised_learning/2_diverse_clustering/D12mean_shift.py
+++ src/AI_models_construction/unsupervised_learning/2_diverse_clustering/D12mean_shift.py
@@ -1,39 +1,23 @@
 # %%
-import pandas as pd
-from sklearn import cluster, preprocessing
 import matplotlib.pyplot as plt
 
-df_wine_all = pd.read_csv(
-    "https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data", header=None)
-df_wine = df_wine_all[[0, 10, 13]]
-df_wine.columns = [u"class", u"color", u"proline"]
-pd.DataFrame(df_wine)
+from init import x, y, z, z_km, labels, cluster_centers
 
-X = df_wine[["color", "proline"]]
-sc = preprocessing.StandardScaler()
-X_norm = sc.fit_transform(X)
-x = X_norm[:, 0]
-y = X_norm[:, 1]
-z = df_wine["class"]
 
+# wine data visualization
 plt.figure(figsize=(10, 3))
 plt.scatter(x, y, c=z)
 plt.show
 
-km = cluster.KMeans(n_clusters=3)
-z_km = km.fit(X_norm)
+
+# k-means
 plt.figure(figsize=(10, 3))
 plt.scatter(x, y, c=z_km.labels_)
 plt.scatter(z_km.cluster_centers_[:, 0], z_km.cluster_centers_[
             :, 1], s=250, marker="*", c="red")
 plt.show
 
-ms = cluster.MeanShift(seeds=X_norm)
-ms.fit(X_norm)
-labels = ms.labels_
-cluster_centers = ms.cluster_centers_
-print(cluster_centers)
-
+# MeanShift
 plt.figure(figsize=(10, 3))
 plt.scatter(x, y, c=labels)
 plt.plot(cluster_centers[0, 0], cluster_centers[0, 1],
---
+++ src/AI_models_construction/unsupervised_learning/2_diverse_clustering/init.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from sklearn import cluster, preprocessing, mixture
+from pyclustering.cluster.xmeans import xmeans
+from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
+
+df_wine_all = pd.read_csv(
+    "https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data", header=None)
+df_wine = df_wine_all[[0, 10, 13]]
+df_wine.columns = [u"class", u"color", u"proline"]
+pd.DataFrame(df_wine)
+
+X = df_wine[["color", "proline"]]
+sc = preprocessing.StandardScaler()
+X_norm = sc.fit_transform(X)
+x = X_norm[:, 0]
+y = X_norm[:, 1]
+z = df_wine["class"]
+
+
+# K-means
+km = cluster.KMeans(n_clusters=3)
+z_km = km.fit(X_norm)
+
+# MeanShift
+ms = cluster.MeanShift(seeds=X_norm)
+ms.fit(X_norm)
+labels = ms.labels_
+cluster_centers = ms.cluster_centers_
+# print(cluster_centers)
+
+# x_means
+xm_c = kmeans_plusplus_initializer(X_norm, 2).initialize()
+xm_i = xmeans(data=X_norm, initial_centers=xm_c, kmax=20, ccore=True)
+xm_i.process()
+
+z_xm = np.ones(X_norm.shape[0])
+for k in range(len(xm_i._xmeans__clusters)):
+    z_xm[xm_i._xmeans__clusters[k]] = k+1
+
+# GMM
+gmm = mixture.GaussianMixture(n_components=3, covariance_type="full")
+z_gmm = gmm.fit(X_norm)
+z_gmm = z_gmm.predict(X_norm)
